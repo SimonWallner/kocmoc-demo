349703a0f3efafad14d7160c407be0a7e5fdfb88
Simon Wallner <me@simonwallner.at>
2010-11-09 11:22:17 +0100

    gray is the new black and quitting works

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,43 @@
 
 using namespace std;
 
+// Test if we got a valid forward compatible context (FCC)
+void test_ogl3(void)
+{
+	GLint profile;
+
+	// retrieve our OpenGL-version
+	std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+
+	// check if we have a core-profile
+	glGetIntegerv(GL_CONTEXT_PROFILE_MASK, &profile);
+	if (profile == GL_CONTEXT_CORE_PROFILE_BIT) {
+		std::cout << "Having a core-profile" << std::endl;
+	} else {
+		std::cout << "Having a compatibility-profile" << std::endl;
+	}
+
+	//gl3w doesn't expose deprecated functions anymore!
+	/*
+	// Try doing a fixed-function operation.
+	// Since this is a FCC, this should create an error.
+	cout << "Doing illegal operation (glTranslate) now..." << endl;
+
+	glGetError(); // Clear error state first..
+	glTranslatef(1,0,0);
+
+	if (glGetError() != GL_NO_ERROR)
+	cout << "Illegal operation caused an error message. :)" << endl;
+	else
+	cerr << "Illegal operation did NOT cause an error message. :(" << endl;
+	*/
+}
+
+void setGLOptions(void)
+{
+	glClearColor(0.69f, 0.65f, 0.63f, 1.0f);
+}
+
 int main(void)
 {
 	cout << "starting KOCMOC, please stand by" << endl;
@@ -30,17 +67,20 @@ int main(void)
 
 	glfwSetWindowTitle("KOCMOC");
 
+	test_ogl3();
+	setGLOptions();
+
 	bool running = true;
 	while (running)
 	{
-//		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-//
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 //		draw(minimal, vao_id);
-//
-//		glfwSwapBuffers();
-//
-//		// Get OGL errors
-//
+
+		glfwSwapBuffers();
+
+		// Get OGL errors
+
 //		get_errors();
 
 		// Check if the window has been closed
@@ -49,6 +89,7 @@ int main(void)
 		running = running && !glfwGetKey( 'Q' );
 		running = running && glfwGetWindowParam( GLFW_OPENED );
 	}
-
-
 }
+
+
+
