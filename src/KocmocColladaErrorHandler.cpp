4071212812375c205c94ab97f0ca1d587d7d4b7d
Simon Wallner <me@simonwallner.at>
2010-11-16 10:48:58 +0100

    renamed the error handler

---
+++ src/KocmocColladaErrorHandler.cpp
@@ -0,0 +1,18 @@
+#include "KocmocColladaErrorHandler.hpp"
+#include "common.hpp"
+
+KocmocColladaErrorHandler::KocmocColladaErrorHandler()
+{
+	// TODO Auto-generated constructor stub
+}
+
+KocmocColladaErrorHandler::~KocmocColladaErrorHandler()
+{
+	// TODO Auto-generated destructor stub
+}
+
+
+bool KocmocColladaErrorHandler::handleError(const COLLADASaxFWL::IError* error)
+{
+	cout << "An error occured: " << error->getFullErrorMessage();
+}
---
+++ src/KocmocColladaErrorHandler.hpp
@@ -0,0 +1,19 @@
+/*
+ * A simple error handler...
+ */
+
+#ifndef KOCMOC_COLLADA_ERROR_HANDLER_HPP_
+#define KOCMOC_COLLADA_ERROR_HANDLER_HPP_
+
+#include "COLLADASaxFWLIErrorHandler.h"
+
+class KocmocColladaErrorHandler :  public COLLADASaxFWL::IErrorHandler
+{
+public:
+		KocmocColladaErrorHandler();
+		virtual ~KocmocColladaErrorHandler();
+
+		virtual bool handleError(const COLLADASaxFWL::IError* error);
+};
+
+#endif /* KOCMOC_COLLADA_ERROR_HANDLER_HPP_ */
