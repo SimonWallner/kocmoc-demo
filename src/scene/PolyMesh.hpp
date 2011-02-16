1e8adffd63f4d99daa032dd0a6ae072c42bc20ac
Simon Wallner <me@simonwallner.at>
2011-02-16 18:56:19 +0100

    started with a rework of the polyMesh interface and internals. More flexible and better. adding arbitrary vertex attributes.

--- src/scene/PolyMesh.hpp
+++ src/scene/PolyMesh.hpp
@@ -5,26 +5,26 @@
 
 #include <renderer/Shader.hpp>
 #include <camera/Camera.hpp>
+#include <Symbol.hpp>
+
+#include <map>
 
 namespace kocmoc
 {
 	namespace scene
 	{
 		/**
-		 * The PolyMesh acts as the cannonical basis for meshes in this application.
+		 * The \c PolyMesh acts as the canonical basis for polygonal meshes.
 		 *
-		 * Data that is generated or imported is transfered into this internal
-		 * representation. It preserves the topological information on a low level basis.
-		 * That is, primitives with vertex indices in a set of shared vertices. Adjacency
-		 * information is not directly available but could be computed from the data.
-		 *
-		 * Data is organized in primitives (points, lines, convex n-gons), vertex indices
-		 * of this primitives and shared vertex attributes (position, etc...).
+		 * A \c polyMesh can hold any number of \b convex polygons. Polygons
+		 * consist of \b vertices that have a position and an arbitrary number
+		 * of named \b vertex attributes like UV-position, color, normal, etc...
+		 * 
+		 * @par It is structured as follows:
+		 * At the heart of the \c PolyMesh is the \c firstIndexArray. It holds 
+		 * the indices of the \c vertxIndexArray where a polygon starts.
 		 */
 
-		// TODO: additional vertex attributes
-		// TODO: Face attributes
-		// TODO: Face-Vertex attributes
 
 		class PolyMesh
 		{
@@ -35,6 +35,40 @@ namespace kocmoc
 				PolyMesh* inside;
 				PolyMesh* outside;
 			};
+
+			/**
+			 * The vertex attribute struct.
+			 * It holds together all the data needed for such an attribute.
+			 */
+			struct vertexAttribute
+			{
+				/** the stride of the attribute. E.g stride=3 for a 3d position
+				 * and stride=2 for UV coordinates */
+				uint stride;
+
+				/** The number of attributes. I.e the length of the \c attributeData
+				 * array. E.g for a constant colored mesh it would be 4 (1xRGBA). */
+				uint attributeDataLength;
+
+				/** The attribute data array */
+				double* attributeData;
+
+				/** Indices for this attribute. Every attribute can have its own
+				 * indices. Its length must match the mesh' \c vertexCount */
+				uint* indices;
+			};
+				
+			typedef std::map<Symbol, vertexAttribute> VertexAttributeMap;
+
+			/** The number of vertices in this mesh.
+			 * A cube for instance has a vertexCount of 24 (4 sides * 4 vertices 
+			 * per plane). However This cube would only have 8 unique vertex
+			 * positions. 
+			 *
+			 * @note the \c vertexCount defines the length of the vertex index
+			 * arrays and it is constant
+			 */
+			const uint vertexCount;
 
 			/**
 			 * Create a new poly mesh. Counts must be known in advance to avoid 
@@ -60,7 +94,7 @@ namespace kocmoc
 
 			/**
 			 * Draw the mesh.
-			 * If the data is not on the gpu yet it will be transfered upon the first call
+			 * If the data is not on the GPU yet it will be transferred upon the first call
 			 */
 			void draw(glm::mat4 parentTransform, camera::Camera *camera, renderer::Shader *shader = NULL);
 
@@ -89,14 +123,17 @@ namespace kocmoc
 
 		private:
 
+			/** The map of vertex attributes */
+			VertexAttributeMap vertexAttributes;
+
+
+
 			/**
-			 * Transfer the data to the gpu, i.e. write the vbos, etc...
+			 * Transfer the data to the GPU, i.e. write the vbos, etc...
 			 */
-			// create and organize renderMesh thingys
+			// create and organize renderMesh thingies
 			void transferData();
 
-			/** total number of vertices **/
-			unsigned int vertexCount;
 
 			/** number of vertex indices **/
 			unsigned int vertexIndexCount;
